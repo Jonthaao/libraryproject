c4928eee4ddf11a9c85f66ecb208ed72d0807d1e
Isadora Souza Borges <isadora.borges@hotmail.com>
2024-08-09 20:12:47 -0300

    Entities

--- library/library/src/main/java/com/library_project/library/Entities/Books.java
+++ library/library/src/main/java/com/library_project/library/Entities/Books.java
@@ -1,5 +1,69 @@
 package com.library_project.library.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Books {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String synopsis;
+    private String title;
+    private String author;
+    private String category;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
--- library/library/src/main/java/com/library_project/library/Entities/Stock.java
+++ library/library/src/main/java/com/library_project/library/Entities/Stock.java
@@ -1,5 +1,32 @@
 package com.library_project.library.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int item_qt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getItem_qt() {
+        return item_qt;
+    }
+
+    public void setItem_qt(int item_qt) {
+        this.item_qt = item_qt;
+    }
 
 }
